9037b092f47c6873e0b33029925750239cd28b4f
alvinchm <chino.9084381@gmail.com>
2024-07-24 00:28:37 -0400

    W5

---
+++ scripts/scripts.js
@@ -0,0 +1,59 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const products = [
+        {
+            id: "fc-1888",
+            name: "flux capacitor",
+            "avg-rating": 4.5
+        },
+        {
+            id: "fc-2050",
+            name: "power laces",
+            averagerating: 4.7
+        },
+        {
+            id: "fs-1987",
+            name: "time circuits",
+            averagerating: 3.5
+        },
+        {
+            id: "ac-2000",
+            name: "low voltage reactor",
+            averagerating: 3.9
+        },
+        {
+            id: "jj-1969",
+            name: "warp equalizer",
+            averagerating: 5.0
+        }
+    ];
+
+    const productSelect = document.getElementById('product');
+
+    // Add the initial disabled option
+    const defaultOption = document.createElement('option');
+    defaultOption.value = "";
+    defaultOption.textContent = "Select a Product ...";
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    productSelect.appendChild(defaultOption);
+
+    // Populate the select element with product options
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.id;  // Using id as the value
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+});
+function setCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+}
+function setLastModified() {
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    setCurrentYear();
+    setLastModified();
+});
